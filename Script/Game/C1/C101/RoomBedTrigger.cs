07e9680ba360e95a1143fcc06195ce3c99f23f12
Limin <3241567720@qq.com>
2019-12-13 18:16:13 +0800

    impl trigger to sleep

---
+++ Script/Game/C1/C101/RoomBedTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBedTrigger : LineTrigger
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (C101Script.isSheCome)
+        {
+            ShowLine.ShowTheLine("去睡觉");
+            isTriggeable = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (C101Script.isSheCome)
+        {
+            ShowLine.ClearTheLine();
+            isTriggeable = false;
+        }
+    }
+
+    private void Update()
+    {
+        // if she is come then can end this scenes
+        if (isTriggeable && C101Script.isSheCome &&
+            (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl)))
+        {
+            Debug.Log("End");
+        }
+    }
+}
