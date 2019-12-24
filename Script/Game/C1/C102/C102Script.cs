a43c9fbbd84f48178133fd3253acd97dfdf4ca78
Limin <3241567720@qq.com>
2019-12-24 21:23:44 +0800

    adjust some line of c102

--- Script/Game/C1/C102/C102Script.cs
+++ Script/Game/C1/C102/C102Script.cs
@@ -13,6 +13,10 @@ public class C102Script : MonoBehaviour
     private static string[] lineList1 = new string[] {
         "昨天晚上，我遇见了死神",
         "她站在我那辆撞坏了的汽车旁边",
+        "我很怕她",
+        "因为我一向以胜利者和幸存者自居",
+        "幸存者都怕死",
+        "正因为怕死，所以我们才活到了现在",
         "我肩膀脱臼，整个身体被困在一堆标价一百五十万克朗的钢材和所谓的“高科技”里"
     };
 
@@ -34,7 +38,6 @@ public class C102Script : MonoBehaviour
         "你对谁负责？上帝还是魔鬼？",
     };
     private static string[] chooseLine2AnsList = new string[] {
-        "哎...",
         "我最讨厌搞关系，只喜欢埋头做事",
         "把那张文件还给我"
     };
@@ -51,6 +54,9 @@ public class C102Script : MonoBehaviour
         LoadBlackLine1();
     }
 
+
+
+    // say the ans
     private void ListenTheLineChoose()
     {
         if (ShowLine.hasResult)
@@ -68,6 +74,9 @@ public class C102Script : MonoBehaviour
         }
     }
 
+
+
+
     private void LoadBlackLine1()
     {
         ShowLine.ShowTheBlackLine("");
@@ -75,10 +84,18 @@ public class C102Script : MonoBehaviour
         Timer.Instance.AddTimerTask(8, () => { ShowLine.ShowTheBlackLine(""); });
         Timer.Instance.AddTimerTask(9, () => { ShowLine.ClearTheBlackLine(); });
         Timer.Instance.AddTimerTask(12, SheNextToMe);
-        Timer.Instance.AddTimerTask(15, () => { ShowLine.ClearTheLine(); });
-        Timer.Instance.AddTimerTask(16, ImStuck);
-        Timer.Instance.AddTimerTask(21, () => { ShowLine.ClearTheLine(); });
-        Timer.Instance.AddTimerTask(23, () => {
+        Timer.Instance.AddTimerTask(17, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(19, ImFearOfHer);
+        Timer.Instance.AddTimerTask(22, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(23, ImSurvive);
+        Timer.Instance.AddTimerTask(28, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(29, FearDead);
+        Timer.Instance.AddTimerTask(33, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(34, WhyWeSurvive);
+        Timer.Instance.AddTimerTask(39, () => { ShowLine.ClearTheLine(); });
+
+        // set ques 1
+        Timer.Instance.AddTimerTask(42, () => {
             ShowLine.SetChooseLine(
                 chooseLineList1[0],
                 chooseLineList1[1],
@@ -88,9 +105,16 @@ public class C102Script : MonoBehaviour
     }
     private void IMetHer() { ShowLine.ShowTheBlackLine(lineList1[0]); }
     private void SheNextToMe() { ShowLine.ShowTheLine(lineList1[1]); }
-    private void ImStuck() { ShowLine.ShowTheLine(lineList1[2]); }
+    private void ImFearOfHer() { ShowLine.ShowTheLine(lineList1[2]); }
+    private void ImSurvive() { ShowLine.ShowTheLine(lineList1[3]); }
+    private void FearDead() { ShowLine.ShowTheLine(lineList1[4]); }
+    private void WhyWeSurvive() { ShowLine.ShowTheLine(lineList1[5]); }
+    private void ImStuck() { ShowLine.ShowTheLine(lineList1[6]); }
 
 
+
+
+    // say the ans 1
     private void LoadChooseAnsC102_1(int ansId)
     {
         chooseC101_1 = ansId;
@@ -100,17 +124,17 @@ public class C102Script : MonoBehaviour
             Timer.Instance.AddTimerTask(2, NoRuleLikeThat);
             Timer.Instance.AddTimerTask(6, () => { ShowLine.ClearTheLine(); });
             Timer.Instance.AddTimerTask(8, ImTransport1);
-            Timer.Instance.AddTimerTask(13, () => { ShowLine.ClearTheLine(); });
+            Timer.Instance.AddTimerTask(15, () => { ShowLine.ClearTheLine(); });
         } else if (ansId == 1)
         {
             Timer.Instance.AddTimerTask(2, NoYourThink);
             Timer.Instance.AddTimerTask(6, () => { ShowLine.ClearTheLine(); });
             Timer.Instance.AddTimerTask(8, ImTransport2);
-            Timer.Instance.AddTimerTask(13, () => { ShowLine.ClearTheLine(); });
+            Timer.Instance.AddTimerTask(15, () => { ShowLine.ClearTheLine(); });
         }
 
-        // set next line
-        Timer.Instance.AddTimerTask(15, () => {
+        // set ques 2
+        Timer.Instance.AddTimerTask(19, () => {
             ShowLine.SetChooseLine(
                 chooseLineList2[0],
                 chooseLineList2[1],
@@ -124,21 +148,29 @@ public class C102Script : MonoBehaviour
     private void ImTransport2() { ShowLine.ShowTheLine(chooseLine1Ans2List[1]); }
 
 
+
+
+
+    // say the ans 2
     private void LoadChooseAnsC102_2(int ansId)
     {
         chooseC101_2 = ansId;
 
-        Timer.Instance.AddTimerTask(3, Aiiiii);
-        Timer.Instance.AddTimerTask(5, () => { ShowLine.ClearTheLine(); });
-        Timer.Instance.AddTimerTask(6, IHeatRelat);
-        Timer.Instance.AddTimerTask(10, () => { ShowLine.ClearTheLine(); });
-        Timer.Instance.AddTimerTask(11, GiveMeFile);
-        Timer.Instance.AddTimerTask(15, () => { ShowLine.ClearTheLine(); });
-        Timer.Instance.AddTimerTask(18, () => { LoadChapter103(); });
+        Timer.Instance.AddTimerTask(5, () => { ShowLine.ShowTheBlackLine(""); });
+        Timer.Instance.AddTimerTask(6, () => { ShowLine.ShowTheBlackLine("她似乎隐瞒了什么"); });
+        Timer.Instance.AddTimerTask(10, () => { ShowLine.ClearTheBlackLine(); });
+
+        Timer.Instance.AddTimerTask(12, IHeatRelat);
+        Timer.Instance.AddTimerTask(16, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(17, GiveMeFile);
+        Timer.Instance.AddTimerTask(20, () => { ShowLine.ClearTheLine(); });
+        Timer.Instance.AddTimerTask(24, () => { LoadChapter103(); });
     }
-    private void Aiiiii() { ShowLine.ShowTheLine(chooseLine2AnsList[0]); }
-    private void IHeatRelat() { ShowLine.ShowTheLine(chooseLine2AnsList[1]); }
-    private void GiveMeFile() { ShowLine.ShowTheLine(chooseLine2AnsList[2]); }
+    private void IHeatRelat() { ShowLine.ShowTheLine(chooseLine2AnsList[0]); }
+    private void GiveMeFile() { ShowLine.ShowTheLine(chooseLine2AnsList[1]); }
+
+
+
 
     private void LoadChapter103()
     {
